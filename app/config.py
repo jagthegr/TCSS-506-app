457ae0a4403e075834e6f8b054750a0d46a7d139
Sean Warlick <warlick.sean@gmail.com>
2025-05-24 16:52:58 -0700

    finished getting postres set up with docker compose.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 class Config:
     SECRET_KEY = "your_secret_key_here"
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql+psycopg2://postgres:password@localhost:5432/prod"
-    )
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:password@db:5432/prod"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     # Add other configuration variables as needed
